546034dfc34ba92274a0ea6f0f4974d2af2649d2
Quentin Smith <quentinsmith3006@gmail.com>
2026-05-10 15:15:41 -0400

    Added C client

--- target/debug/minipipe.d
+++ target/debug/minipipe.d
@@ -1,1 +1,1 @@
-/Users/quentinsmith/Desktop/projects/minipipe/target/debug/minipipe: /Users/quentinsmith/Desktop/projects/minipipe/src/lib.rs /Users/quentinsmith/Desktop/projects/minipipe/src/main.rs
+/Users/quentinsmith/Desktop/projects/server/target/debug/minipipe: /Users/quentinsmith/Desktop/projects/server/src/lib.rs /Users/quentinsmith/Desktop/projects/server/src/main.rs
